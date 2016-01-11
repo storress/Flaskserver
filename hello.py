68ea50d08eea7793419daf860c5b52bb23e14b56
salomon <nomolas1@gmail.com>
2016-01-11 16:06:14 -0300

    First server up

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Probando + cambios'
+
+if __name__=='__main__':
+    app.debug = True
+    app.run()
