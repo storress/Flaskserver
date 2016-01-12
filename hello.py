ac5f7d605cb9564e2cd21af52e19f96cd9ce9246
salomon <nomolas1@gmail.com>
2016-01-12 16:36:10 -0300

    Add echo method

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 
 
 @app.route('/')
-def hello_world():
-    return 'Probando + cambios'
+def index():
+    return 'index'
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.json
+    return str(data)
 
 if __name__=='__main__':
     app.debug = True
